1847d3317457aad18061c2a0e61ae181505adf9c
Josh DeWitt <chessami92@gmail.com>
2013-06-22 11:59:45 -0500

    Added a test for the getOutput function.

--- backprop.c
+++ backprop.c
@@ -1,1 +1,36 @@
+#include <math.h>
 #include "backprop.h"
+
+float getOutput( float weightedSum ) {
+    return 2.0 / ( 1.0 + exp( -weightedSum ) ) - 1.0;
+}
+
+float weightedSum( Layer *inputLayer, Layer *currentLayer ) {
+/*    float returnVal = 0;
+    int i;
+
+    for( i = 0; i < inputs->elements; ++i ) {
+        returnVal += inputs->a[i] * weights->a[i];
+    }
+
+    return returnVal;*/
+}
+
+/*void updateWeights( TestCase *testCase, Vector *weights ) {
+    float learningRate = 5;
+    float in;
+    float actualOutput;
+    float error;
+    float derivative;
+    int i;
+
+    in = weightedSum( testCase->inputs, weights );
+    actualOutput = getOutput( in );
+    error = testCase->desiredOutput - actualOutput;
+    derivative = 1 / ( 2 * powf( cosh( in / 2 ), 2 ) );
+
+    for( i = 0; i < weights->elements; ++i ) {
+        weights->a[i] = weights->a[i] + learningRate * error * derivative * testCase->inputs->a[i];
+    }
+}*/
+
--- backprop_test.c
+++ backprop_test.c
@@ -1,5 +1,28 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+#include "backprop.h"
+#include "random.h"
+
+static void getOutput_test( void ) {
+    float output;
+    int i;
+
+    for( i = -20; i <= 20; ++i ) {
+        output = getOutput( i );
+        if( i < 0 ) {
+            assert( output < 0 && "Output should be negative" );
+        } else if ( i > 0 ) {
+            assert( output > 0 && "Output should be positive" );
+        } else {
+            assert( output == 0 && "Output should be zero" );
+        }
+    }
+}
 
 int main( void ) {
+    getOutput_test();
+
     return EXIT_SUCCESS;
 }
