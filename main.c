b037e4c35f2eecc9f3451f4783d191e958bcdcbd
Josh DeWitt <chessami92@gmail.com>
2013-06-23 16:14:55 -0500

    Now processing the command line arguments.

--- main.c
+++ main.c
@@ -3,19 +3,36 @@
 #include "backprop.h"
 #include "netBuilder.h"
 
-int main( int argc, char *argv[] ) {
-    char trainFlag;
+char trainingFlag;
+FILE *definitionFile;
+FILE *inputFile;
 
+int processArguments( int argc, char *argv[] ) {
     if( argc < 4 ) {
-        fprintf( stderr, "Usage: main [-r][-t] [node definition file] [input file]\n" );
-        return EXIT_FAILURE;
+        return 0;
     }
 
     if( strcmp( argv[1], "-t" ) == 0 ) {
-        trainFlag = 1;
+        trainingFlag = 1;
     } else if( strcmp( argv[1], "-r" ) == 0 ) {
-        trainFlag = 0;
+        trainingFlag = 0;
     } else {
+        return 0;
+    }
+
+    definitionFile = fopen( argv[2], "r+" );
+    inputFile = fopen( argv[3], "r" );
+
+    if( !definitionFile || !inputFile ) {
+        return 0;
+    }
+
+    return 1;
+}
+
+int main( int argc, char *argv[] ) {
+    if( !processArguments( argc, argv ) ) {
+        fprintf( stderr, "Usage: main [-r, -t] [node definition file] [input file, training file]\n" );
         return EXIT_FAILURE;
     }
 
