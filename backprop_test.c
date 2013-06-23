9e725d128e6ebfaf4fffec850885916ef954b458
Josh DeWitt <chessami92@gmail.com>
2013-06-22 19:10:23 -0500

    Added a second output node to two tests.
    Better tested the forward and backward propagation functions.

--- backprop_test.c
+++ backprop_test.c
@@ -41,38 +41,40 @@ static void weightedSumsAndOutput_test( void ) {
 
 static void forwardPropagate_test() {
     float hiddenWeights[2][2] = {{-1, 1}, {1, -1}};
-    float outputWeights[2] = {0.5, 0.25};
+    float outputWeights[2][2] = {{0.5, 0.25}, {0.25, -0.5}};
     Node inputNodes[2];
     Node hiddenNodes[2] = {{hiddenWeights[0]}, {hiddenWeights[1]}};
-    Node outputNode = {outputWeights};
+    Node outputNode[2] = {{outputWeights[0]}, {outputWeights[1]}};
     Layer inputLayer = {inputNodes, 2};
     Layer hiddenLayer = {hiddenNodes, 2};
-    Layer outputLayer = {&outputNode, 1};
+    Layer outputLayer = {outputNode, 2};
 
     inputLayer.nodes[0].output = 0.5; inputLayer.nodes[1].output = -0.5;
     forwardPropagate( &inputLayer, &hiddenLayer, &outputLayer );
     assert( fabs( -0.1881307 - outputLayer.nodes[0].output ) < 0.001 && "Output is not as expected" );
+    assert( fabs( -0.5162368 - outputLayer.nodes[1].output ) < 0.001 && "Output is not as expected" );
 }
 
 static void backPropagate_test( void ) {
     float hiddenWeights[2][2] = {{-1, 1}, {1, -1}};
-    float outputWeights[2] = {0.5, 0.25};
+    float outputWeights[2][2] = {{0.5, 0.25}, {0.25, -0.5}};
     Node inputNodes[2];
     Node hiddenNodes[2] = {{hiddenWeights[0]}, {hiddenWeights[1]}};
-    Node outputNode = {outputWeights};
-    Node desired = {NULL, -0.5};
+    Node outputNode[2] = {{outputWeights[0]}, {outputWeights[1]}};
+    Node desired[2] = {{NULL, -0.5}, {NULL, -0.5}};
     Layer inputLayer = {inputNodes, 2};
     Layer hiddenLayer = {hiddenNodes, 2};
-    Layer outputLayer = {&outputNode, 1};
-    Layer desiredLayer = {&desired, 1};
+    Layer outputLayer = {outputNode, 2};
+    Layer desiredLayer = {desired, 2};
 
     inputLayer.nodes[0].output = 0.5; inputLayer.nodes[1].output = -0.5;
     forwardPropagate( &inputLayer, &hiddenLayer, &outputLayer );
     backPropagate( &hiddenLayer, &outputLayer, &desiredLayer );
 
-    assert( fabs( -0.3118693 - outputLayer.nodes[0].error ) < 0.001 && "Output node error is not as expected" );
-    assert( fabs( -0.1559347 - hiddenLayer.nodes[0].error ) < 0.001 && "First node of hidden layer's output is not as expected" );
-    assert( fabs( -0.0779673 - hiddenLayer.nodes[1].error ) < 0.001 && "Second node of hidden layer's output is not as expected" );
+    assert( fabs( -0.3118693 - outputLayer.nodes[0].error ) < 0.001 && "First output node error is not as expected" );
+    assert( fabs( 0.0162368 - outputLayer.nodes[1].error ) < 0.001 && "Second output node error is not as expected" );
+    assert( fabs( -0.1518755 - hiddenLayer.nodes[0].error ) < 0.001 && "First node of hidden layer's output is not as expected" );
+    assert( fabs( -0.0860857 - hiddenLayer.nodes[1].error ) < 0.001 && "Second node of hidden layer's output is not as expected" );
 }
 
 int main( void ) {
